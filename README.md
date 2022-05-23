2e4878f04adf51975289b371b1df50747b232d26
Peng Tsinv <32813194+TuringCodeOne@users.noreply.github.com>
2022-05-23 09:48:38 +0800

    feat: 完成上传文件

--- README.md
+++ README.md
@@ -16,6 +16,11 @@
  * Running on http://127.0.0.1:5000/
 ```
 
+如果你想开启热重启模式，请使用
+```shell
+flask run --debugger --reload
+```
+
 + Others
 
 可以参考[Flask Quick Start](https://flask.palletsprojects.com/en/2.1.x/quickstart/)
--- main.py
+++ main.py
@@ -5,26 +5,19 @@ import os
 
 IMG_EXT = ['jpg', 'jpeg', 'png']
 
-# def formatFileName():
-# ErrorMsg = {
-#     FileNotFound: "你没有上传文件"
-# }
+UPLOAD_FOLDER = './source'
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# 保存文件
+# 静态文件路径
 uploads_dir = os.path.join(os.getcwd(), 'uploads')
 os.makedirs(uploads_dir, exist_ok=True)
 
-# 保存文件
-def saveFileToServer(file):
-    file.save(os.path.join(app.config['UPLOAD_FOLDER']), 'test')
-
-UPLOAD_FOLDER = './source'
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 
 @app.route('/upload', methods=['POST'])
 def uploadHandler():
     return jsonify([{'a': 1, 'c': 3, 'b': 2, 'e': 5, 'd': 4}])
+
 
 @app.route('/file/upload', methods=['POST'])
 def uploadFile():
@@ -32,6 +25,6 @@ def uploadFile():
     fds = request.files
     if not len(fds):
         return fail('失败')
-    for fKey in fds.values():
-        fKey.save(os.path.join(uploads_dir), secure_filename('test'))
+    for file in fds.values():
+        file.save(os.path.join(uploads_dir, file.filename))
     return success({'success': True}, '成功')
